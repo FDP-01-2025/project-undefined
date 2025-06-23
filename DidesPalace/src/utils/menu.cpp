d9c43e8035db18ac4a699a47a0f397a0ae30f9f3
FranciscoG1803 <00077225@uca.edu.sv>
2025-06-22 21:56:17 -0600

    Agregar diseño a menu

--- DidesPalace/src/utils/menu.cpp
+++ DidesPalace/src/utils/menu.cpp
@@ -2,32 +2,74 @@
 #include "utils/consoleUtils.h"
 #include <iostream>
 #include <windows.h>
+#include <string>
+
+#define WINDOW_WIDHT 80
+#define WINDOW_HEIGHT 25
 
 using namespace std;
 
-void showMenu() {
+void drawBackground() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    // Estilo verde brillante
-    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    for (int y = 0; y < WINDOW_HEIGHT; ++y) {
+        for (int x = 0; x < WINDOW_WIDHT; ++x) {
+            moveCursor(x, y);
 
-    string title = "=== MENU PRINCIPAL ===";
+            if ((x + y) % 17 == 0) {
+                // Color para estrellas (#fff3b0 → amarillo claro)
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+                cout << "*";
+            } else if (x == 0 || x == WINDOW_WIDHT - 1) {
+                // Muros verticales (#335c67 → azul oscuro)
+                SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+                cout << "|";
+            } else if (y == 0 || y == WINDOW_HEIGHT - 1) {
+                // Muros horizontales
+                SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+                cout << "-";
+            } else {
+                cout << " ";
+            }
+        }
+    }
+}
+
+void showMenu() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    drawBackground();
+
+    // Texto en rojo fuerte (#9e2a2b)
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+
+    string titleArt[] = {
+        "  ____ ___ ____  ____  _____ ____  _____      _    ____ ___  ",
+        " |  _ \\_ _|  _ \\|  _ \\| ____|  _ \\| ____|    / \\  |  _ \\_ _| ",
+        " | | | | || | | | | | |  _| | | | |  _|     / _ \\ | | | | |  ",
+        " | |_| | || |_| | |_| | |___| |_| | |___   / ___ \\| |_| | |  ",
+        " |____/___|____/|____/|_____|____/|_____| /_/   \\_\\___/___|  ",
+        "                       DIDESPALACE                           "
+    };
+
+    int y = 3;
+    for (const string& line : titleArt) {
+        moveCursor((WINDOW_WIDHT - line.length()) / 2, y++);
+        cout << line;
+    }
+
     string opt1 = "1. Empezar";
     string opt2 = "ESC. Salir del juego";
-    string prompt = "Seleccione una opcion: ";
+    string prompt = "";
 
-    int y = 5;  // línea donde empiezas
+    y += 2;
 
-    moveCursor((WINDOW_HEIGHT - title.size()) / 2, y);
-    cout << title;
-
-    moveCursor((WINDOW_HEIGHT - opt1.size()) / 2, y + 2);
+    moveCursor((WINDOW_WIDHT - opt1.size()) / 2, y++);
     cout << opt1;
 
-    moveCursor((WINDOW_HEIGHT - opt2.size()) / 2, y + 3);
+    moveCursor((WINDOW_WIDHT - opt2.size()) / 2, y++);
     cout << opt2;
 
-    moveCursor((WINDOW_HEIGHT - prompt.size()) / 2, y + 5);
+    moveCursor((WINDOW_WIDHT - prompt.size()) / 2, y++);
     cout << prompt;
 
     // Restaurar color normal
