5c1260a7095d9135c4a362a248abe4b29398ad23
EduGuerra1 <00043625@uca.edu.sv>
2025-06-26 22:21:12 -0600

    Estructura de las preguntas (lista de numeros) para el nivel 3

--- DidesPalace/src/miniGames/3_numberSort.cpp
+++ DidesPalace/src/miniGames/3_numberSort.cpp
@@ -15,8 +15,10 @@ namespace
 {
     // Number list
     const vector<pair<string, string>> LISTA = {
-        {"3, 1, 2, 4", "1, 2, 3, 4"},
-        {"2, 8, 4, 6", "2, 4, 6, 8"}};
+        {"Ordena los siguientes numeros: 3, 1, 2, 4", "1, 2, 3, 4"},
+        {"Ordena los siguientes numeros: 5, 3, 7, 1", "1, 3, 5, 7"},
+        {"Ordena los siguientes numeros: 20, 10, 5, 15", "5, 10, 15, 20"},
+        {"Ordena los siguientes numeros: 2, 8, 4, 6", "2, 4, 6, 8"}};
 
     // Variable para llevar el progreso
     int currentQuestionIndex = 0;
